fc810131edaed0ab08c411c9b32fde2453a0b0d5
MartinT <m@rtin.win>
2024-11-30 14:52:30 +0100

    Intergrated LazyGit into Neovim

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,21 @@
+return {
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{
+			"<leader>git",
+			"<cmd>LazyGit<cr>",
+			desc = "Open Lazy[GIT]",
+		},
+	},
+}
